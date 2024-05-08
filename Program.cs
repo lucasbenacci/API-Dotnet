7b25f51d145ff17d587865d087cf72f2dee0e0d9
Lucas Souza <90305152+lucasbenacci@users.noreply.github.com>
2024-05-08 19:17:02 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AgendaContext>(options =>
-    options.UseSqlServer("Server=localhost\\sqlexpress;Initial Catalog=Agenda; Integrated Security=True"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
